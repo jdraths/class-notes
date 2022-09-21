33b18eac2116b0da6730efa292e5efca048962bd
Roane <jdraths@alumni.nd.edu>
2022-09-21 13:07:13 -0500

    a very basic sidenav -  no mobile support (#13)
    * basic sidenav
    
    * fix link

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     },
   },
   plugins: [
+    `gatsby-plugin-emotion`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -63,6 +64,7 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          `gatsby-remark-autolink-headers`,
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
---
+++ src/components/toc.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+/**
+ * A Table of Contents component reading headings from graphQL
+ * @param {*} headings 
+ * @returns 
+ */
+const ToC = ({ headings }) => (
+  <aside id="ToC-container" className="sidebar">
+    <ul id="ToC">
+      <h2 id="Title">Table of contents</h2>
+      <div id="InnerScroll">
+        {headings.map(heading => {
+          if (heading.depth > 4) {
+            return <div />
+          }
+
+          return (
+            <li className="ToCElement" key={heading.value}>
+              <a className="ToCLink"
+                href={`#${heading.value.replace(/\s+/g, "-").toLowerCase()}`}
+              >
+                {heading.value}
+              </a>
+            </li>
+          )
+        })}
+      </div>
+    </ul>
+  </aside>
+)
+
+
+export default ToC
